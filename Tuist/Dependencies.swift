12232c336d5f918485df010e8f0197a522400f1b
Łukasz Śliwiński <sliwinski.lukas@gmail.com>
2023-10-20 08:54:32 +0200

    Thanks to Stefano Mondino - https://github.com/tuist/tuist/issues/5320#issuecomment-1771684714 run-time issue fixed

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,6 +3,9 @@ import ProjectDescription
 let spm = SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/aws-amplify/amplify-swift", requirement: .upToNextMajor(from: "2.15.1"))
 ], targetSettings: [
+    "Amplify": [
+        "GENERATE_MASTER_OBJECT_FILE": "YES"
+    ],
     "AmplifyBigInteger": [
         "OTHER_SWIFT_FLAGS": "-Xcc -Wno-error=non-modular-include-in-framework-module",
         "HEADER_SEARCH_PATHS": .array([
